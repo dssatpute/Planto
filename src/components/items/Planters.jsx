1a8f037e37a70dc382613df0296fc0fb42499ecb
dssatpute <dnyaneshss99@gmail.com>
2021-10-19 19:32:45 +0530

    commit

--- src/components/items/Planters.jsx
+++ src/components/items/Planters.jsx
@@ -4,28 +4,44 @@ import styles from "./planters.module.css";
 import { planters } from "../../data/planters";
 
 const Planters = () => {
+    
   return (
     <>
       <Header />
       <div className={styles.main}>
         <div className={styles.heading}>
-          Your home garden will look much better with various types of planters
-          available. Whether you want to hang plants on the wall or have them
-          placed in pots and boxes, you will find the perfect planter to suit
-          your liking.
+          <h2>Planter</h2>
+          <span>
+            Your home garden will look much better with various types of
+            planters available.
+          </span>
         </div>
-        <div className={styles.items}>
-          {planters.map((planter) => (
-            <div className={styles.card}>
-              <div className={styles.image_div}>
-                <img className={styles.image} src={planter.image}></img>
-              </div>
-              <div className={styles.content}>
-                <h2>{planter.price}</h2>
-                <span>{planter.title}</span>
-              </div>
+        <div className={styles.division}>
+          <div className={styles.filter}>
+            <span>Filters</span>
+            <div className="price-filter">
+              <span>From</span>
+              <span>To</span>
             </div>
-          ))}
+          </div>
+          <div className={styles.items}>
+            {planters.map((planter) => (
+              <div className={styles.card}>
+                <a onClick={show}>
+                  <div className={styles.image_div}>
+                    <img className={styles.image} src={planter.image}></img>
+                  </div>
+                </a>
+                <div className={styles.content}>
+                  <h3>{planter.price}</h3>
+                  <span style={{ fontSize: "0.9rem" }}>{planter.title}</span>
+                </div>
+                {/* <div style={{height:'auto'}}>
+                    <button className={styles.buy_button}>Buy Now</button>
+                </div> */}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
