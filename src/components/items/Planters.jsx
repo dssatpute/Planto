4b3f412584f4d41dd3fd4424b4420895ee30dbbf
dssatpute <dnyaneshss99@gmail.com>
2021-10-19 16:22:52 +0530

    commit

--- src/components/items/Planters.jsx
+++ src/components/items/Planters.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import Header from '../landing/Header';
+import React from "react";
+import Header from "../landing/Header";
+import styles from "./planters.module.css";
+import { planters } from "../../data/planters";
 
 const Planters = () => {
-    return (
-        <div>
-            <Header/>
+  return (
+    <>
+      <Header />
+      <div className={styles.main}>
+        <span>
+          Your home garden will look much better with various types of planters
+          available. Whether you want to hang plants on the wall or have them
+          placed in pots and boxes, you will find the perfect planter to suit
+          your liking.
+        </span>
+        <div className={styles.items}>
+          {planters.map((planter) => (
+            <div className={styles.card}>
+              <div className={styles.image_div}>
+                <img className={styles.image} src={planter.image}></img>
+              </div>
+              <div className={styles.content}>
+                <h2>{planter.price}</h2>
+                <span>{planter.title}</span>
+              </div>
+            </div>
+          ))}
         </div>
-    );
-}
+      </div>
+    </>
+  );
+};
 
 export default Planters;
---
+++ src/data/planters.js
@@ -0,0 +1,37 @@
+export const planters=[
+    {
+        title:'Stone Shaped Planter',
+        price:'₹ 200',
+        image:require('../assets/images/planters/stone_finish.jpg').default
+    },
+    {
+        title:'Stone Shaped Planter',
+        price:'₹ 200',
+        image:require('../assets/images/planters/stone_finish.jpg').default
+    },
+    {
+        title:'Stone Shaped Planter',
+        price:'₹ 200',
+        image:require('../assets/images/planters/stone_finish.jpg').default
+    },
+    {
+        title:'Stone Shaped Planter',
+        price:'₹ 200',
+        image:require('../assets/images/planters/stone_finish.jpg').default
+    },
+    {
+        title:'Stone Shaped Planter',
+        price:'₹ 200',
+        image:require('../assets/images/planters/stone_finish.jpg').default
+    },
+    {
+        title:'Stone Shaped Planter',
+        price:'₹ 200',
+        image:require('../assets/images/planters/stone_finish.jpg').default
+    },
+    {
+        title:'Stone Shaped Planter',
+        price:'₹ 200',
+        image:require('../assets/images/planters/stone_finish.jpg').default
+    }
+]
