b8efcd05bd8ac4027f73ce3d2bb3d9f1b619470e
dssatpute <dnyaneshss99@gmail.com>
2021-10-19 19:33:05 +0530

    initial

--- src/data/planters.js
+++ src/data/planters.js
@@ -5,33 +5,38 @@ export const planters=[
         image:require('../assets/images/planters/stone_finish.jpg').default
     },
     {
-        title:'Stone Shaped Planter',
+        title:'3 inch (8 cm) Grower Round Plastic Pot (Black) (set of 6)',
         price:'₹ 200',
-        image:require('../assets/images/planters/stone_finish.jpg').default
+        image:require('../assets/images/planters/black_planter.jpg').default
     },
     {
-        title:'Stone Shaped Planter',
+        title:'Coir Germination Tray (8 Cells, Square) (Set of 5)',
         price:'₹ 200',
-        image:require('../assets/images/planters/stone_finish.jpg').default
+        image:require('../assets/images/planters/coil_germination.jpg').default
     },
     {
-        title:'Stone Shaped Planter',
+        title:'4 inch (10 cm) Round Plastic Thermoform Pot (Terracotta Color) (set of 20)',
         price:'₹ 200',
-        image:require('../assets/images/planters/stone_finish.jpg').default
+        image:require('../assets/images/planters/thermofom.png').default
     },
     {
-        title:'Stone Shaped Planter',
+        title:'2.5 inch (6 cm) Square Glass Vase',
         price:'₹ 200',
-        image:require('../assets/images/planters/stone_finish.jpg').default
+        image:require('../assets/images/planters/square_glass.png').default
     },
     {
-        title:'Stone Shaped Planter',
+        title:'4 inch (10 cm) Round Terrarium Glass Vase without Rim (4.5 Inch (11.4 cm) Ht.)',
         price:'₹ 200',
-        image:require('../assets/images/planters/stone_finish.jpg').default
+        image:require('../assets/images/planters/round_terrarium.png').default
     },
     {
-        title:'Stone Shaped Planter',
+        title:'4 inch (10 cm) Round Terrarium Glass Vase without Rim (4.5 Inch (11.4 cm) Ht.)',
         price:'₹ 200',
-        image:require('../assets/images/planters/stone_finish.jpg').default
+        image:require('../assets/images/planters/round_terrarium.png').default
+    },
+    {
+        title:'4 inch (10 cm) Round Terrarium Glass Vase without Rim (4.5 Inch (11.4 cm) Ht.)',
+        price:'₹ 200',
+        image:require('../assets/images/planters/round_terrarium.png').default
     }
 ]
