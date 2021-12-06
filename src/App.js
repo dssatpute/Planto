5a5f91d6eed2d9da1d9550832125fa7303919a0c
dssatpute <dnyaneshss99@gmail.com>
2021-12-06 22:07:37 +0530

    validation

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ import {
   Cart,
   CheckOut,
   OrderPlaced,
-  Footer
+  Footer,
+  Payment,
 } from "./index";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const user = useAuth();
-  // console.log(user);
   useEffect(() => {
     setLoading(user.loading);
   }, [user]);
@@ -33,7 +33,7 @@ function App() {
             <Navbar user={user} />
             <Switch>
               <Route path="/" exact>
-                <Landing user={user}  loading={loading}/>
+                <Landing user={user} loading={loading} />
               </Route>
               <Route path="/login">
                 <SignIn />
@@ -41,18 +41,24 @@ function App() {
               <Route path="/signup">
                 <CreateAccount />
               </Route>
-              <Route path="/products/get-clicked-item/:category/:productId" exact>
+              <Route
+                path="/products/get-clicked-item/:category/:productId"
+                exact
+              >
                 <ProductDetails user={user} />
               </Route>
               <Route path="/products/:category" exact>
                 <ProductCategory />
               </Route>
 
-              <Route path="/cart-items">
+              <Route path="/cart-items" exact>
                 <Cart user={user} />
               </Route>
-              <Route path="/check-out">
-                <CheckOut user={user} exact />
+              <Route path="/check-out" exact>
+                <CheckOut user={user}  />
+              </Route>
+              <Route path="/payment" exact>
+                <Payment user={user} />
               </Route>
               <Route path="/placed-order/:userid" exact>
                 <OrderPlaced user={user} />
